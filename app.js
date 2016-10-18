ddbe9fe21f77d38e9d017f61d7e4f181fd643915
TheatreKnitter <ecolemansound@gmail.com>
2016-10-18 18:49:11 -0400

    basic function complete

--- app.js
+++ app.js
@@ -8,25 +8,38 @@ var getMusic = function(bands) {
 		url:"https://www.tastekid.com/api/similar?q=" + bands + "&type=music&k=244514-FindNuMu-JLIXASE2&limit=10&info=1",
 		q: "bands",
 		info: 1,
-		//dataType: "jsonp",
 		jsonpCallback: "logResults"
 		
-	});
+	})
 
+	.done(function(result){ 
+		$.each(result.Similar.Results, function(i, item) {
+			var user = showMusic(item);
+			$('.results').append(user);
+		});
+	});
 };
 
 var showMusic = function(music){
-	var result = $('.templates').clone();
-	var  musicElem = result.find('.artist');
-	var decription = result.find('.description');
-	var youtube = result.find('.youtube');
+	var template = $('.templates').clone();
+	template.find('.results').append('<dt>Artist:</dt>').append('<dd class="artist"></dd>');
+	template.find('.artist').text(music.Name);
+	template.find('.results').append('<dt>Description:</dt>').append('<dd class="description"></dd>');
+	template.find('.description').text(music.wTeaser);
+	template.find('.results').append('<dt>youTube link:</dt>').append('<dd class="youtube"></dd>');
+	template.find('.youtube').html('<a target="_blank" '+'href=' + music.yUrl + '>Take me to the Video!</a>');
+
+	$(".search").append(template);
+	template.removeClass("templates");
+	template.removeClass("hidden");
 };
 
 
 function logResults(json){
 		console.log(json);
-		//console.log(json.array[0].name);
-	};
+};
+
+
 
 $(document).ready(function(){
 	$('.collect').submit(function(e){
